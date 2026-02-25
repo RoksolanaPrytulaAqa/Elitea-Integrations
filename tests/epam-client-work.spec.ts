b6f1893320adf94fe7575377d32ab407e7eb5787
RoksolanaPrytulaAqa <roksja@gmail.com>
2026-02-25 15:09:56 +0200

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+// EPAM Client Work test - initial scaffold
+test('EPAM: placeholder - Client Work navigation', async ({ page }) => {
+  // Placeholder - test body will be added in subsequent incremental update
+});
