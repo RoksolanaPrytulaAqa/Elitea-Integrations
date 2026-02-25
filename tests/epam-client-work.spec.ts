301bac598d9941b76c333edbcba5f5fae130ac73
RoksolanaPrytulaAqa <roksja@gmail.com>
2026-02-25 15:10:28 +0200

    Delete tests/epam-client-work.spec.ts

--- tests/epam-client-work.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-// EPAM Client Work test - initial scaffold
-test('EPAM: placeholder - Client Work navigation', async ({ page }) => {
-  // Placeholder - test body will be added in subsequent incremental update
-});
